6cc2031f0761d292ecfe0687b6e271fd330731ce
Maksym Semehen <semehen.devops@proton.me>
2026-06-29 22:10:33 +0300

    SKEL-38-update-run-infrastructure-bash

--- run-infrastructure.sh
+++ run-infrastructure.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+vagrant destroy -f
+for vm in $(VBoxManage list vms | awk -F'"' '{print $2}'); do VBoxManage unregistervm "$vm" --delete; done
+rm -rf output-golden-image
+rm -rf .vagrant
+rm -rf VirtualBox\ VMs/*
+
 packer init .
 
 packer build .
@@ -9,3 +15,5 @@ packer build .
 vagrant box add --force ubuntu26-golden-image ./output-golden-image/package.box
 
 vagrant up
+
+ansible-playbook ./jenkins-configuration/setup_jenkins.yml
