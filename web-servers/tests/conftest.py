0dec4dd5550fdc3e580c270902efaff8f5f11c02
Apeshito <148392402+DenisFolyush@users.noreply.github.com>
2026-07-13 17:18:13 +0300

    SKEL-77-Disable database setup in conftest.py
    Comment out database setup and teardown in test fixture.

--- web-servers/tests/conftest.py
+++ web-servers/tests/conftest.py
@@ -14,11 +14,11 @@ def test_app():
         "WTF_CSRF_ENABLED": False,
         "SECRET_KEY": "test_secret"
     })
-    with app.app_context():
+    # with app.app_context():
 
-        db.create_all()    
-        yield app
-        db.drop_all()
+    #     db.create_all()    
+    #     yield app
+    #     db.drop_all()
 
 
 @pytest.fixture()
