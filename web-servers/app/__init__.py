c2d838541f3d7104bf3965085d008960a3d86056
Oleh <konovalukoleg0@gmail.com>
2026-08-30 14:59:34 +0300

    SKEL2-88-changed-s3-config

--- web-servers/app/__init__.py
+++ web-servers/app/__init__.py
@@ -29,6 +29,10 @@ app.s3_client = boto3.client(
         region_name=os.environ.get("AWS_REGION", "eu-north-1"),
         aws_access_key_id=os.environ.get("AWS_ACCESS_KEY"),
         aws_secret_access_key=os.environ.get("AWS_SECRET_KEY"),
+        config=Config(
+        signature_version="s3v4",
+        s3={"addressing_style": "virtual"}
+        ),
     )
     BUCKET_NAME = os.environ.get("S3_BUCKET")
 
