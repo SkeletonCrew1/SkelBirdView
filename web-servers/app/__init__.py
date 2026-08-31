99399938df59de2d3cf9cb0c208f793f4774fbac
Oleh <konovalukoleg0@gmail.com>
2026-08-31 14:19:36 +0300

    SKEL2-88-fixing-error-307

--- web-servers/app/__init__.py
+++ web-servers/app/__init__.py
@@ -12,34 +12,37 @@ region = os.environ.get("AWS_REGION", "eu-north-1")
 
 
 def create_app():
-    global s3, BUCKET_NAME
-
     app = Flask(__name__)
-    load_dotenv()
 
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-    app.config[
-        "SQLALCHEMY_DATABASE_URI"
-    ] = f"mysql+pymysql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"mysql+pymysql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}"
+        f"@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
+    )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
     db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = "app.login"
 
-    
     region = os.environ.get("AWS_REGION", "eu-north-1")
-    BUCKET_NAME = os.environ.get("S3_BUCKET")
+    bucket_name = os.environ.get("S3_BUCKET")
+
+
+    app.config["S3_BUCKET"] = bucket_name
     app.s3_client = boto3.client(
         "s3",
         region_name=region,
-        endpoint_url=f"https://s3.{region}.amazonaws.com",
+        endpoint_url=f"https://s3.{region}.amazonaws.com",  
         aws_access_key_id=os.environ.get("AWS_ACCESS_KEY"),
         aws_secret_access_key=os.environ.get("AWS_SECRET_KEY"),
-        config=Config(signature_version="s3v4"),
+        config=Config(
+            signature_version="s3v4",
+            s3={"addressing_style": "virtual"}
+        ),
     )
 
     from .routes import app as app_blueprint
-
     app.register_blueprint(app_blueprint)
 
     return app
--- web-servers/app/routes.py
+++ web-servers/app/routes.py
@@ -20,24 +20,16 @@ def get_presigned_url(filename):
         return None
     try:
         
-        s3_client = boto3.client(
-            "s3",
-            region_name=os.environ.get("AWS_REGION", "eu-north-1"),
-            aws_access_key_id=os.environ.get("AWS_ACCESS_KEY"),
-            aws_secret_access_key=os.environ.get("AWS_SECRET_KEY"),
-            config=Config(
-                signature_version="s3v4",
-                s3={"addressing_style": "virtual"}
-            ),
-        )
+        s3_client = current_app.s3_client
+        bucket_name = current_app.config["S3_BUCKET"]
+
         return s3_client.generate_presigned_url(
             ClientMethod="get_object",
             Params={
-                "Bucket": os.environ.get("S3_BUCKET"),
+                "Bucket": bucket_name,
                 "Key": filename,
             },
             ExpiresIn=3600,
-            HttpMethod="GET",
         )
     except Exception as e:
         print(f"Presign error: {e}")
